2b2a8758ea5703186c0c9e5eff871070f0580fdf
vitalpanda <51076330+vitalpanda@users.noreply.github.com>
2019-05-28 06:34:06 +0600

    Add files via upload

---
+++ precache-manifest.844e9e49290ddf7c4563b2f72a501e07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fac75bfccbab2e6ab207cdab635814",
+    "url": "/firstrReact/index.html"
+  },
+  {
+    "revision": "2f45419c04112a8d9bc1",
+    "url": "/firstrReact/static/css/main.cd8f7dc2.chunk.css"
+  },
+  {
+    "revision": "734c948d99fcc657a2a0",
+    "url": "/firstrReact/static/js/2.00399540.chunk.js"
+  },
+  {
+    "revision": "2f45419c04112a8d9bc1",
+    "url": "/firstrReact/static/js/main.55d9e3f0.chunk.js"
+  },
+  {
+    "revision": "34d8c078e9a81c04cb59",
+    "url": "/firstrReact/static/js/runtime~main.9df7aa91.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/firstrReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/firstrReact/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/firstrReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/firstrReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/firstrReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
